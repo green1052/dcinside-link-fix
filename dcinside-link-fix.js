d4d11fb0dba5176e5a8c2c58042566b1cdcf61e0
green1052 <cydyellowgreen@gmail.com>
2020-11-03 20:55:07 +0900

    for문 도배 와!

--- dcinside-link-fix.js
+++ dcinside-link-fix.js
@@ -10,18 +10,34 @@
 // @run-at		document-end
 // ==/UserScript==
 
-(function () {
+(async function () {
     "use strict";
 
     const regex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
-    document.querySelectorAll(".writing_view_box > div > div").forEach(query => {
-        const text = regex.exec(query.textContent)[0];
-        query.innerHTML = query.innerHTML.replace(text, `<a href="${text}" target="_blank">${text}</a>`)
-    });
+    for (const query of document.querySelectorAll(".writing_view_box div")) {
+        const children = query.children;
 
-    document.querySelectorAll(".writing_view_box > div > p").forEach(query => {
-        const text = regex.exec(query.textContent)[0];
-        query.innerHTML = query.innerHTML.replace(text, `<a href="${text}" target="_blank">${text}</a>`)
-    });
+        for (let i = 0; i < children.length; i++) {
+            const children2 = children[i].children;
+
+            for (let j = 0; j < children2.length; j++) {
+                if (children2[j].tagName === "A")
+                    continue;
+
+                const textSplit = children2[j].textContent.split(' ');
+
+                for (const string of textSplit) {
+                    const exec = regex.exec(string);
+
+                    if (!exec)
+                        continue;
+
+                    const text = exec[0];
+
+                    children2[j].innerHTML = children2[j].innerHTML.replace(text, `<a class="__hoverBox_aLink" href="${string}">${string}</a>`);
+                }
+            }
+        }
+    }
 })();
